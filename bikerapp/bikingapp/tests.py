1744e634bc6e4c53928fc0879cf93ad8d338996e
rutuj-anturkar <77196495+rutuj-anturkar@users.noreply.github.com>
2022-10-30 14:29:29 -0400

    Update tests.py

--- bikerapp/bikingapp/tests.py
+++ bikerapp/bikingapp/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+
+        self.assertIs(False, False)
