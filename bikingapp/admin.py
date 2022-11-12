97dc32955f2d4f71d13cd88231e079447dede054
Abhinav <42210327+takhilabhinav@users.noreply.github.com>
2022-11-12 17:06:39 -0500

    Merge branch 'develop' into user_abhinav

--- bikingapp/admin.py
+++ bikingapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Event, FriendMgmt
+from .models import Event, FriendMgmt,Comment
 
 """
 , Snippet
@@ -8,6 +8,15 @@ from .models import Event, FriendMgmt
 
 admin.site.register(Event)
 admin.site.register(FriendMgmt)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
 # admin.site.register(Snippet)
 
 # admin.site.register(Snippet)
