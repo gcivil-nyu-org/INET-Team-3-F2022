d80a376ec0ee8b680f9f69b17e8287f2b16a6d87
Abhinav <42210327+takhilabhinav@users.noreply.github.com>
2022-11-16 23:57:58 -0500

    Update tests.py

--- bikingapp/tests.py
+++ bikingapp/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm
 
 
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         self.assertIs(False, False)
@@ -92,94 +93,8 @@ class QuestionModelTests(TestCase):
         )
         event.save()
         self.assertEqual(user.username, event.created_by)
-    
-    def test_was_published_recently_with_future_question(self):
-        self.assertIs(False, False)
 
-    def test_home_page_loads_properly(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_browse_events_loads_properly(self):
-        response = self.client.get("/browse_events")
-        self.assertEqual(response.status_code, 200)
-
-    def test_map_loads_properly(self):
-        response = self.client.get("/map")
-        self.assertEqual(response.status_code, 200)
-
-    def test_register_user_loads_properly(self):
-        response = self.client.get("/register_user")
-        self.assertEqual(response.status_code, 200)
-
-    def test_event_has_correct_author(self):
-        """Events are given created_by correctly when saving"""
-        event = Event.objects.create(
-            title="test event", location="test location", description="test description"
-        )
-        event.created_by = "John Doe"
-        event.save()
-        self.assertEqual("John Doe", event.created_by)
-
-    def test_event_has_correct_title(self):
-        """Events are given title correctly when saving"""
-        event = Event.objects.create(
-            title="test event", location="test location", description="test description"
-        )
-        event.save()
-        self.assertEqual("test event", event.title)
-
-    def test_event_has_correct_location(self):
-        """Events are given title correctly when saving"""
-        event = Event.objects.create(
-            title="test event", location="test location", description="test description"
-        )
-        event.save()
-        self.assertEqual("test location", event.location)
-
-    def test_bookmarking_event_possible_user(self):
-        """Check if bookmarking an event is working with specific user"""
-        event = Event.objects.create(
-            title="test event", location="test location", description="test description"
-        )
-        event.save()
-        user = User.objects.create_user(username="testuser", password="12345")
-        # login = self.client.login(username="testuser", password="12345")
-        bookmark_event = BookmarkEvent.objects.create(user=user, event=event)
-        bookmark_event.save()
-        self.assertEqual(user, bookmark_event.user)
-
-    def test_bookmarking_event_possible_event(self):
-        """Check if bookmarking an event is working with specific event"""
-        event = Event.objects.create(
-            title="test event", location="test location", description="test description"
-        )
-        event.save()
-        user = User.objects.create_user(username="testuser", password="12345")
-        # login = self.client.login(username="testuser", password="12345")
-        bookmark_event = BookmarkEvent.objects.create(user=user, event=event)
-        bookmark_event.save()
-        self.assertEqual(event, bookmark_event.event)
-
-    def test_create_user(self):
-        """test if account is being created"""
-        user = User.objects.create_user(username="testuser", password="12345")
-        user.save()
-        get_user = User.objects.get(username="testuser")
-        self.assertEqual("testuser", get_user.username)
-
-    def test_create_event_after_creating_user(self):
-        """test if a new user is able to create events"""
-        user = User.objects.create_user(username="testuser", password="12345")
-        user.save()
-        event = Event.objects.create(
-            title="test event",
-            location="test location",
-            description="test description",
-            created_by=user.username,
-        )
-        event.save()
-        self.assertEqual(user.username, event.created_by)
+# Create your tests hereclass TestIsUserAuth(TestCase):
     def setUp(self):
         self.form =SignupForm
         self.user = User.objects.create_user(
@@ -221,7 +136,8 @@ class QuestionModelTests(TestCase):
             }
         )
         self.assertFormError(
-            form, "username", "A user with that username already exists." )
+            form, "username", "A user with that username already exists."
+        )
 
     # def test_cant_login_with_email_that_is_taken(self):
     #
