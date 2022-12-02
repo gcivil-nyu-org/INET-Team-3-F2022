5f9887c39429804be85d90b7a97426d5c6541d1d
Your Name <rutuj.anturkar@gmail.com>
2022-12-01 22:10:46 -0800

    remove login bug from comments

--- bikingapp/views.py
+++ bikingapp/views.py
@@ -282,7 +282,7 @@ def profile(request, username):
                 friend_username = form.cleaned_data["friend_username"]
                 if (
                     models.CustomUser.objects.filter(username=friend_username).first()
-                    is not None
+                    is not None and friend_username != request.user.username
                 ):
                     obj = models.FriendMgmt(
                         from_user=request.user,
