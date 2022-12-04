7cc62a94f865e8058a948e15febd8556d485f087
Kieran Bissessar <bissessk@outlook.com>
2022-12-04 09:10:03 -0500

    [BIKE-118] env variables

--- bikerapp/settings.py
+++ bikerapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,7 +131,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  # new
 EMAIL_HOST = "smtp.gmail.com"  # new
 EMAIL_PORT = 587  # new
 EMAIL_HOST_USER = "mg060894nyu@gmail.com"  # new
-EMAIL_HOST_PASSWORD = "frgcszmlqokngjrc"  # new
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # new
 EMAIL_USE_TLS = True  # new
 
 # ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
