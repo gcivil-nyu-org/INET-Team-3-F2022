ca7ac0edecc93041165acb92096e5eb93500719d
Kieran Bissessar <bissessk@outlook.com>
2022-12-10 00:18:39 -0500

    [BIKE-MAINT] Add Public Events to Map

--- bikingapp/static/js/map.js
+++ bikingapp/static/js/map.js
@@ -2,6 +2,7 @@ const btn1BikeLane = document.getElementById("btn1");
 const btn2BikePark = document.getElementById("btn2");
 const btn3BikeColl = document.getElementById("btn3");
 const btn4RepIssue = document.getElementById("btn4");
+const btn5SeeEvnts = document.getElementById("btn5");
 
 /*
   BUTTON 1: Bike Lanes
@@ -13,9 +14,11 @@ btn1BikeLane.addEventListener("click", function onClick() {
   btn2BikePark.classList.remove("btn-primary");
   btn3BikeColl.classList.remove("btn-primary");
   btn4RepIssue.classList.remove("btn-primary");
+  btn5SeeEvnts.classList.remove("btn-primary");
   btn2BikePark.classList.add("btn-secondary");
   btn3BikeColl.classList.add("btn-secondary");
   btn4RepIssue.classList.add("btn-secondary");
+  btn5SeeEvnts.classList.add("btn-secondary");
 
   const uluru = { lat: 40.7237, lng: -73.9898 };
 
@@ -50,9 +53,11 @@ btn2BikePark.addEventListener("click", function onClick() {
   btn1BikeLane.classList.remove("btn-primary");
   btn3BikeColl.classList.remove("btn-primary");
   btn4RepIssue.classList.remove("btn-primary");
+  btn5SeeEvnts.classList.remove("btn-primary");
   btn1BikeLane.classList.add("btn-secondary");
   btn3BikeColl.classList.add("btn-secondary");
   btn4RepIssue.classList.add("btn-secondary");
+  btn5SeeEvnts.classList.add("btn-secondary");
 
   const uluru = { lat: 40.7237, lng: -73.9898 };
 
@@ -80,9 +85,11 @@ btn3BikeColl.addEventListener("click", function onClick() {
   btn1BikeLane.classList.remove("btn-primary");
   btn2BikePark.classList.remove("btn-primary");
   btn4RepIssue.classList.remove("btn-primary");
+  btn5SeeEvnts.classList.remove("btn-primary");
   btn1BikeLane.classList.add("btn-secondary");
   btn2BikePark.classList.add("btn-secondary");
   btn4RepIssue.classList.add("btn-secondary");
+  btn5SeeEvnts.classList.add("btn-secondary");
 
   const uluru = { lat: 40.7237, lng: -73.9898 };
 
@@ -133,7 +140,6 @@ btn3BikeColl.addEventListener("click", function onClick() {
 /*
   BUTTON 4: Reported Lane Issues
 */
-// write code here
 btn4RepIssue.addEventListener("click", function onClick() {
   btn4RepIssue.classList.remove("btn-secondary");
   btn4RepIssue.classList.add("btn-primary");
@@ -150,12 +156,6 @@ btn4RepIssue.addEventListener("click", function onClick() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: uluru,
-  });
-
-  map.data.setStyle(function (feature) {
-    return {
-      icon: markerpng,
-    }
   });
 
   let locations = [];
@@ -184,7 +184,6 @@ btn4RepIssue.addEventListener("click", function onClick() {
           position: results[0].geometry.location,
         });
 
-        //var contentStr = issueInfo['title'] + issueInfo['content'] + issueInfo['author'] + issueInfo['location'];
         const contentStr =
           '<div id="content">' +
           '<div id="siteNotice">' +
@@ -211,5 +210,85 @@ btn4RepIssue.addEventListener("click", function onClick() {
       }
     });
   });
+
+});
+
+/*
+  BUTTON 5: Events
+*/
+btn5SeeEvnts.addEventListener("click", function onClick() {
+  btn5SeeEvnts.classList.remove("btn-secondary");
+  btn5SeeEvnts.classList.add("btn-primary");
+  btn3BikeColl.classList.remove("btn-secondary");
+  btn1BikeLane.classList.remove("btn-primary");
+  btn2BikePark.classList.remove("btn-primary");
+  btn3BikeColl.classList.remove("btn-primary");
+  btn4RepIssue.classList.remove("btn-primary");
+  btn1BikeLane.classList.add("btn-secondary");
+  btn2BikePark.classList.add("btn-secondary");
+  btn3BikeColl.classList.add("btn-secondary");
+  btn4RepIssue.classList.add("btn-secondary");
+
+  const uluru = { lat: 40.7237, lng: -73.9898 };
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: uluru,
+  });
+
+  let locations = [];
+  for (let i = 0; i < publicObjsJson.length; i++) {
+    locations.push(publicObjsJson[i]['fields']['location']);
+  }
+
+  let publicEventInfos = [];
+  for (let i = 0; i < publicObjsJson.length; i++) {
+    publicEventInfos.push(publicObjsJson[i]['fields']);
+  }
+
+  var geocoder;
+  geocoder = new google.maps.Geocoder();
+  var markers_temp = []
+  var markerClusterer = new MarkerClusterer(map, markers_temp,
+    {zoomOnClick: false, imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  // Add some markers to the map.
+  var markers = locations.map((location, i) => {
+    const publicEventInfo = publicEventInfos[i % publicEventInfos.length];
+
+    geocoder.geocode({ 'address': location }, function (results, status) {
+      if (status === 'OK') {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+        });
+
+        const contentStr =
+          '<div id="content">' +
+          '<div id="siteNotice">' +
+          "</div>" +
+          '<h4 id="firstHeading" class="firstHeading">'+ publicEventInfo['title']+'</h4>' +
+          '<div id="bodyContent">' +
+          "<p>"+ publicEventInfo['description'] + "</p>" +
+          "<p>Event Type: "+ publicEventInfo['event_type'] + "</p>" +
+          '<p>Author: '+ publicEventInfo['created_by'] +'</p>' +
+          '<p>Location: '+ publicEventInfo['location'] +'</p>' +
+          "</div>" +
+          "</div>";
+        var infoWindow = new google.maps.InfoWindow({
+          content: contentStr
+        });
+        marker.addListener("click", () => {
+          infoWindow.open(map, marker);
+        });
+        markers_temp.push(marker)
+        markerClusterer.addMarker(marker);
+      } 
+      
+      else {
+        console.log(location+': Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  });
+
 
 });
